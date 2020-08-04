e974736475dfc5db2842971b6fa72d8d6dd6ed30
Mart Ratas <mart.ratas@gmail.com>
2020-08-04 16:11:46 +0300

    Added falling head effect

---
+++ src/main/java/dev/ratas/openeffecttest/effects/FallingHeadEffect.java
@@ -0,0 +1,63 @@
+package dev.ratas.openeffecttest.effects;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FallingHeadEffect implements SubEffect {
+    private final JavaPlugin plugin;
+    private final float anglePer;
+    private final int repeats;
+    private final double yOffset;
+    private final double fallPer;
+    private int repeated = 0;
+    private Runnable whenDone;
+
+    // public FallingHeadEffect(JavaPlugin plugin, float rotate, int repeats, double yOffset) {
+    //     this(plugin, rotate, repeats, yOffset);
+    // }
+
+    public FallingHeadEffect(JavaPlugin plugin, float anglePer, double fallPer, double fallingDistance, double yOffset) {
+        this.plugin = plugin;
+        this.anglePer = anglePer;
+        this.fallPer = fallPer;
+        this.repeats = (int) (fallingDistance / fallPer);
+        this.yOffset = yOffset;
+    }
+
+    @Override
+    public void play(Location location) {
+        location = location.clone().add(0, yOffset, 0);
+        ArmorStand as = location.getWorld().spawn(location, ArmorStand.class);
+        // FallingBlock block = location.getWorld().spawnFallingBlock(location, data);
+        // block.setDropItem(false);
+        as.setVisible(false);
+        as.setGravity(false);
+        as.setInvulnerable(true);
+        as.setBasePlate(false);
+        as.getEquipment().setHelmet(new ItemStack(Material.PLAYER_HEAD));
+        // block.
+        plugin.getServer().getScheduler().runTaskTimer(plugin, (t) -> {
+            if (repeated++ > repeats) {
+                t.cancel();
+                as.remove();
+                if (whenDone != null) {
+                    whenDone.run();
+                }
+                return;
+            }
+            Location tele = as.getLocation();
+            tele.add(0, -fallPer, 0);
+            as.teleport(tele);
+            as.setRotation(anglePer * repeated, 0.0F);
+        }, 1L, 1L);
+    }
+
+	@Override
+	public void whenDone(Runnable runnable) {
+		whenDone = runnable;
+	}
+    
+}
--- src/main/java/dev/ratas/openeffecttest/effects/ScytheExampleEffect.java
+++ src/main/java/dev/ratas/openeffecttest/effects/ScytheExampleEffect.java
@@ -15,17 +15,32 @@ public class ScytheExampleEffect extends AbstractEffect {
 
         Vector vector = new Vector(1, 0, 1);
         double yOffset = 0.0D;
-        for (int nr = 0; nr < 200; nr++) {
-            vector = vector.multiply(0.99);
+        double step = 0.015;
+        for (int nr = 0; nr < 94; nr++) { // 2^0.5 = 1.41/0.015 = 94.28
+            vector = vector.clone();//.multiply(0.99);
+            double len = vector.length();
+            double newLen = len - step;
+            vector = vector.normalize().multiply(newLen);
             yOffset += 0.01;
             addStagePointedTowards(vector, yOffset);
             vector = vector.rotateAroundY(ROTATE);
         }
+        addFallingBlockStage(yOffset);
     }
 
     private void addStagePointedTowards(Vector vector, double yOffset) {
         List<SubEffect> stage = new ArrayList<>();
         stage.add(new ScytheEffect(plugin, 20, 0.6, 10, vector.clone(), yOffset));
+        addStage(stage);
+    }
+
+    private void addFallingBlockStage(double yOffset) {
+        List<SubEffect> stage = new ArrayList<>();
+                                     // JavaPlugin plugin, float anglePer, double fallPer, double fallingDistance, double yOffset) {
+        double fallper = 0.15D;
+        float rotate = (float) ROTATE * 30F;
+        double fallDistance = yOffset + 1.75D;
+        stage.add(new FallingHeadEffect(plugin, rotate, fallper, fallDistance, yOffset));
         addStage(stage);
     }
 
