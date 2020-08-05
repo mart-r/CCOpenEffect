eed0357a9acf9ba6e1ab7a25996e48e1e5753259
Mart Ratas <mart.ratas@gmail.com>
2020-08-05 08:24:55 +0300

    Mixed the two example effects together

--- src/main/java/dev/ratas/openeffecttest/effects/ScytheExampleEffect.java
+++ src/main/java/dev/ratas/openeffecttest/effects/ScytheExampleEffect.java
@@ -3,8 +3,11 @@ package dev.ratas.openeffecttest.effects;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Particle;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
+
+import dev.ratas.openeffecttest.effects.SpiralEffect.Axis;
 
 public class ScytheExampleEffect extends AbstractEffect {
     private static final double ROTATE = 2 * Math.PI / 20;
@@ -27,6 +30,7 @@ public class ScytheExampleEffect extends AbstractEffect {
             vector = vector.rotateAroundY(ROTATE);
         }
         addFallingBlockStage(yOffset);
+        addCircles();
     }
 
     private void addStagePointedTowards(Vector vector, double yOffset) {
@@ -42,6 +46,34 @@ public class ScytheExampleEffect extends AbstractEffect {
         double fallDistance = yOffset;
         stage.add(new FallingHeadEffect(plugin, rotate, fallper, fallDistance, yOffset - 1.75D));
         addStage(stage);
+    }
+    private static final Particle PARTICLE = Particle.FLAME;
+    private static final Particle PARTICLE_2 = Particle.CRIT_MAGIC;
+    private static final double RADIUS = 0.7D;
+    private static final double SLOPE = 0.05D;
+    private static final int PER_CIRCLE = 30;
+    private static final double MAX_HEIGHT = 1.2D;
+    private static final int WAIT_TIME = 80;
+    private static final int PARTICLES_AT_ONCE = 5;
+    private static final double AXIS_OFFSET = 0.5D;
+
+    private void addCircles() {
+        List<SubEffect> stage1 = new ArrayList<>();
+        stage1.add(new SpiralEffect(plugin, PARTICLE, RADIUS, SLOPE, PER_CIRCLE, MAX_HEIGHT, WAIT_TIME,
+                PARTICLES_AT_ONCE, Axis.X, AXIS_OFFSET));
+        stage1.add(new SpiralEffect(plugin, PARTICLE, RADIUS, SLOPE, PER_CIRCLE, MAX_HEIGHT, WAIT_TIME,
+                PARTICLES_AT_ONCE, Axis.Y, AXIS_OFFSET));
+        stage1.add(new SpiralEffect(plugin, PARTICLE, RADIUS, SLOPE, PER_CIRCLE, MAX_HEIGHT, WAIT_TIME,
+                PARTICLES_AT_ONCE, Axis.Z, AXIS_OFFSET));
+        stage1.add(new SpiralEffect(plugin, PARTICLE, RADIUS, -SLOPE, PER_CIRCLE, MAX_HEIGHT, WAIT_TIME,
+                PARTICLES_AT_ONCE, Axis.X, -AXIS_OFFSET));
+        stage1.add(new SpiralEffect(plugin, PARTICLE, RADIUS, -SLOPE, PER_CIRCLE, MAX_HEIGHT, WAIT_TIME,
+                PARTICLES_AT_ONCE, Axis.Z, -AXIS_OFFSET));
+        addStage(stage1);
+        List<SubEffect> stage2 = new ArrayList<>();
+        stage2.add(new SpiralEffect(plugin, PARTICLE_2, RADIUS * 2, SLOPE * 2, PER_CIRCLE, MAX_HEIGHT, WAIT_TIME,
+                PARTICLES_AT_ONCE, Axis.Y, AXIS_OFFSET));
+        addStage(stage2);
     }
 
 }
